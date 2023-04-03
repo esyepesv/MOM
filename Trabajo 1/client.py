b2b349522e28c15915271b2eb856ed39669c7a42
yachaverrc <60229713+yachaverrc@users.noreply.github.com>
2023-04-03 18:40:48 -0500

    Primer Codigo Yhilmar
    Un intento base, hay un cliente sencillo y un server sencillo, fallos en el archivo MOM

---
+++ Trabajo 1/client.py
@@ -0,0 +1,15 @@
+import grpc
+
+import message_pb2
+import message_pb2_grpc
+
+def run():
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = message_pb2_grpc.MessageServiceStub(channel)
+        nombre = input("Ingresa un mensaje: ")
+        response = stub.Greet(message_pb2.MessageRequest(name = nombre))
+        print(response.greeting)
+        
+if __name__ == '__main__':
+    run()
+
---
+++ Trabajo 1/server.py
@@ -0,0 +1,28 @@
+from concurrent import futures
+import grpc
+
+import message_pb2
+import message_pb2_grpc
+
+class MessageServicer(message_pb2_grpc.MessageServiceServicer):
+    def Greet(self, request, context):
+
+        print(request.name)
+
+        response = message_pb2.MessageResponse()
+
+        response.greeting = "Hello, {}!".format(request.name)
+        return response
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    message_pb2_grpc.add_MessageServiceServicer_to_server(MessageServicer(), server)
+    server.add_insecure_port("[::]:50051")
+    print("Hola, Soy El Server")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
