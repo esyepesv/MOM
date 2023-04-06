837a8c48fc8f5d4d24166d9ad6ecde38e48ce638
root <root@DESKTOP-V1AO4B2.localdomain>
2023-04-06 11:31:10 -0500

    conexion grpc cliente-mom-servidor

--- mom-server/mom.py
+++ mom-server/mom.py
@@ -1,1 +1,52 @@
-#
+from concurrent import futures
+import grpc
+import sys
+sys.path.append('../')
+import message_pb2
+import message_pb2_grpc
+
+from collections import deque
+
+colaEntrada = deque()
+colaSalida = deque()
+
+class MessageServicer(message_pb2_grpc.MessageServiceServicer):
+
+    def Greet(self, request, context):     
+
+        response = message_pb2.MessageResponse()
+
+        colaEntrada.append(request.name)
+
+        print(colaEntrada)
+
+        send()
+
+
+def listen():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    message_pb2_grpc.add_MessageServiceServicer_to_server(MessageServicer(), server)
+    server.add_insecure_port("[::]:50052")
+    server.start()
+    print("Hola, Soy El MOM")
+    server.wait_for_termination()
+
+
+def send():
+     with grpc.insecure_channel('localhost:50051') as channel:
+            
+            stub = message_pb2_grpc.MessageServiceStub(channel)
+
+            response = stub.Greet(message_pb2.MessageRequest(name = colaEntrada.pop()))
+
+            response = message_pb2.MessageResponse()
+
+            colaSalida.append(response.greeting)
+
+            print("esto hay en la cola de salida: ", colaSalida)
+
+            return response
+
+
+if __name__ == "__main__":
+    listen()
