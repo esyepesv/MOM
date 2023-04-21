87dc60b065ce6607df0c01c921d3a6b1b5c3fe1f
root <root@DESKTOP-V1AO4B2.localdomain>
2023-04-20 20:00:47 -0500

    ajuste runtime

--- mom/mom/FLASK.py
+++ mom/mom/FLASK.py
@@ -182,5 +182,5 @@ def get_messages_user_fl():
     return jsonify(res), 201
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(port=4000)
+    app.run(host='0.0.0.0', port=4000, debug=True)
+
